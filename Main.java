69701ad16dd6a9cd5bea6ccf5872907cd55ff669
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-11-04 02:59:28 +0000

    envio

--- Main.java
+++ Main.java
@@ -1,5 +1,12 @@
 class Main {
   public static void main(String[] args) {
     System.out.println("Gott Mit Uns");
+    teste.nome();
+  }
+}
+
+class teste {
+  public static void nome () {
+    System.out.println("Nome");
   }
 }
