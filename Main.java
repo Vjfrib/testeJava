8a4add9fe8c8d7346790c6b3ea951477eeb4bca0
Vjfrib <107771171+Vjfrib@users.noreply.github.com>
2023-11-06 01:05:50 +0000

    on e off

--- Main.java
+++ Main.java
@@ -2,11 +2,19 @@ class Main {
   public static void main(String[] args) {
     System.out.println("Gott Mit Uns");
     teste.nome();
+    calma();
   }
+
+  private static void calma() {
+    //Exemplo de metodo interno (function like)
+    System.out.println("Calma");
+  }
+  
 }
 
 class teste {
   public static void nome () {
+    //Como externo msm
     System.out.println("Nome");
   }
 }
